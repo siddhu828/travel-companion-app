680591de6c40f4bf5abd6553029f4959be7ccb77
siddhu828 <145180939+siddhu828@users.noreply.github.com>
2025-06-30 09:52:46 +0530

    removed .env/client and VITE_API_URL temporarily

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://travel-companion-app-1nns.onrender.com",
 });
 
 export default api;
